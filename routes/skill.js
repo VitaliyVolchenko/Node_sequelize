1f0f80d1a022e631e0696a00f61cbb00225076d2
vitaliyvolchenko <vitaliyvolchenkodeveloper@gmail.com>
2018-12-14 09:57:42 +0200

    Added Access-Control-Allow-.. and JWT route protection(middleware)

--- routes/skill.js
+++ routes/skill.js
@@ -4,15 +4,17 @@ const skillsController = require('../controllers/skills');
 
 const router = express.Router();
 
+const checkAuth = require('../middleware/check-auth');
+
 
 router.get('/', skillsController.getAddSkills);
 
 router.get('/:id', skillsController.getAddSkill);
 
-router.post('/', skillsController.postAddSkill);
+router.post('/', checkAuth, skillsController.postAddSkill);
 
-router.put('/:id', skillsController.putAddSkill);
+router.put('/:id', checkAuth, skillsController.putAddSkill);
 
-router.delete('/:id', skillsController.deleteAddSkill);
+router.delete('/:id', checkAuth, skillsController.deleteAddSkill);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -18,6 +18,18 @@ app.use(bodyParser.json());
 app.use('/uploads', express.static('uploads'));
 // app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 // app.set('view engine', 'handlebars')
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 // Routes
 app.use('/divides', divideRoutes);
